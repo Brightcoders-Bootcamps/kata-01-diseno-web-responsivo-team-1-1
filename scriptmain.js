95b7646c071976fcfc8369b89f1b7531804f7a76
candelarioavalos <candelario.avalos@hotmail.com>
2020-09-03 12:51:34 -0500

    Terminando Vista Movil.

--- scriptmain.js
+++ scriptmain.js
@@ -1,8 +1,11 @@
 function deploying(){
     var anchor = document.getElementsByClassName("navbar-item");
+    var anchor = document.getElementsByClassName("navbar-container");
 
     for(var i = 0; i < anchor.length; i++){ //vector con la cantidad de elementos del menú
         console.log("funciona?");
         anchor[i].classList.toggle("hide"); //mostrar los elementos del menú
     }
+
+    //$('hide').toggle();
 }
