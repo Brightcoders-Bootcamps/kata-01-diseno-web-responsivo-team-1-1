70d7c5992991dd82f8a3667656d28e906c36b72a
EduJimenez96 <ejimenez10@ucol.mx>
2020-09-07 12:10:50 -0500

    Modificacion en boton para acortar links >

--- scriptmain.js
+++ scriptmain.js
@@ -1,22 +1,35 @@
+//ocultar/mostrar menú en la versión móvil
 function deploying(){
-    var anchor = document.getElementsByClassName("navbar-container");
+    var menu = document.getElementsByClassName("navbar-container");
 
-    for(var i = 0; i < anchor.length; i++){
+    //console.log("funciona?");
+    menu[0].classList.toggle("hide");
+}
+
+function boxText(){
+    if(addlink==false){
         console.log("funciona?");
-        anchor[i].classList.toggle("hide"); //mostrar el menú
     }
 }
 
+//Botón para acortar links
 function addlink(){
     var texto = $('.shorter').val();
     
 
-    if(texto === 'Shorter a link here...'){
-        //document.write("please add a link");
-        console.log("esto es una prueba");
+    if(texto === 'Shorter a link here...'){ //validar que en la caja de texto se haya escrito un link
+        //console.log("esto es una prueba");
 
-        document.getElementsByClassName("hide2").style.display="block";
+        document.getElementsByClassName("hide2")[0].style.display = 'block';
+        document.getElementsByClassName("shorter")[0].style.color = 'hsl(0, 73%, 89%)';
+        document.getElementsByClassName("shorter")[0].style.border = '3px solid hsl(0, 87%, 67%)';
 
         return false;
+    } else{
+        document.getElementsByClassName("hide2")[0].style.display = 'none';
+        document.getElementsByClassName("shorter")[0].style.color = 'hsl(0, 3%, 69%)';
+        document.getElementsByClassName("shorter")[0].style.border = '0px solid #ffffff';
+        const prueba = document.getElementByClassName("shorter")[0].value;
     }
+
 }
