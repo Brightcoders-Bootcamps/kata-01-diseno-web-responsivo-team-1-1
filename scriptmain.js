79fca3c5b6a490ad776bf83dd1227803961006e2
EduJImenez96 <ejimenez10@ucol.mx>
2020-09-09 12:37:37 -0500

    Desktop design changes and API function Co-authored-by: BrendaVielmas <breen.vielmas@gmail.com> Co-authored-by: candelarioavalos <candelario.avalos@hotmail.com>

--- scriptmain.js
+++ scriptmain.js
@@ -6,11 +6,12 @@ function deploying(){
     menu[0].classList.toggle("hide");
 }
 
-function boxText(){
+
+/*function boxText(){
     if(addlink==false){
         console.log("funciona?");
     }
-}
+}*/
 
 //Botón para acortar links
 function addlink(){
@@ -18,18 +19,57 @@ function addlink(){
     
 
     if(texto === 'Shorter a link here...'){ //validar que en la caja de texto se haya escrito un link
-        //console.log("esto es una prueba");
+        console.log("esto es una prueba");
 
         document.getElementsByClassName("hide2")[0].style.display = 'block';
         document.getElementsByClassName("shorter")[0].style.color = 'hsl(0, 73%, 89%)';
         document.getElementsByClassName("shorter")[0].style.border = '3px solid hsl(0, 87%, 67%)';
+
 
         return false;
     } else{
         document.getElementsByClassName("hide2")[0].style.display = 'none';
         document.getElementsByClassName("shorter")[0].style.color = 'hsl(0, 3%, 69%)';
         document.getElementsByClassName("shorter")[0].style.border = '0px solid #ffffff';
-        const prueba = document.getElementByClassName("shorter")[0].value;
+        //const prueba = document.getElementByClassName("shorter")[0].value;
     }
+    //esto es sólo para probar
+
+    var long_url = document.getElementsByClassName("shorter")[0].value; //se toma el link ingresado en el textbox
+
+    $.postJSON( //mandamos el link
+        "https://rel.ink/api/links/", 
+        {
+            url: longurl
+        },function(){
+
+        }
+    );
+
+    $.getJSON( //recibimos el link acortado
+        "https://rel.ink/api/links/Nn8y9p/", 
+        { 
+            hashid:"Nn8y9p",
+            url: long_url,
+            created_at:"2019-06-18T21:29:57.922801Z"
+        },
+        function(response)
+        {
+            alert('Shortened link is: ' + response.data.url);
+        }
+    );
+
+    /*var objeto = {
+        nombre: 'API',
+        num: 12,
+        estado: true,
+        subojeto: {
+            subnombre: 'a',
+            num: 19,
+            estado: false
+        }
+    }
+
+    console.log(objeto.subojeto);*/
 
 }
